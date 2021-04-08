10a21bb6d51dd4a8044db2cdaa75dde931eb5d93
lil58 <lemonie.li@dell.com>
2021-04-08 14:22:44 +0800

    add ratelimit

--- src/main/java/io/pravega/perf/WriterWorker.java
+++ src/main/java/io/pravega/perf/WriterWorker.java
@@ -169,7 +169,7 @@ public abstract class WriterWorker extends Worker implements Callable<Void> {
         while (msElapsed < msToRun) {
             for (int i = 0; (msElapsed < msToRun) && (i < EventsPerFlush); i++) {
                 time = recordWrite(payload, stats::recordTime);
-                rateLimiter.acquire();
+                rateLimiter.acquire(1);
 //                eCnt.control(cnt++, time);
 //                msElapsed = time - startTime;
             }
