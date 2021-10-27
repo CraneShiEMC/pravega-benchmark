b069a0fc0c632f37bb33199135b5a77df1168848
mmll <lemonie.li@dell.com>
2021-10-27 15:38:45 +0800

    update create

--- src/main/java/io/pravega/perf/PravegaPerfTest.java
+++ src/main/java/io/pravega/perf/PravegaPerfTest.java
@@ -493,7 +493,7 @@ public class PravegaPerfTest {
                 newRdGrpName = streamName + "RG";
                 streamHandle = new PravegaStreamHandler(scopeName, newCreateStream, newRdGrpName, controllerUri, segmentCount,
                         segmentScaleKBps, segmentScaleEventsPerSecond, scaleFactor, TIMEOUT, controller, bgExecutor, createScope);
-                    streamHandle.create();
+                if(streamHandle.create()){
                     EventStreamWriter<byte[]> newProducer = factory.createEventWriter(newCreateStream,
                     new ByteArraySerializer(),
                     EventWriterConfig.builder()
@@ -507,7 +507,7 @@ public class PravegaPerfTest {
                     log.info("-------------- day stream {} created-------------------",newCreateStream);
                     producerList.add(newProducer);
                 }
-            
+            }
         }
 
         private AtomicLong[] getAtomicNum() {
