b4e0d9c8df682643181fbe003ba57fd0452429ee
mmll <lemonie.li@dell.com>
2021-10-29 18:44:46 +0800

    update

--- src/main/java/io/pravega/perf/WriterWorker.java
+++ src/main/java/io/pravega/perf/WriterWorker.java
@@ -151,7 +151,7 @@ public abstract class WriterWorker extends Worker implements Callable<Void> {
         for (int i = 0; i < events; i++) {
             //byte[] data = createPayload();
             if(isBatch){
-                recordWrite(createPayload(eventsPerSec*batchSize), stats::recordTime);
+                recordWrite(payload, stats::recordTime);
             }
             else{
                 recordWrite(payload, stats::recordTime);
@@ -183,7 +183,7 @@ public abstract class WriterWorker extends Worker implements Callable<Void> {
         while ((time - startTime) < msToRun) {
             //byte[] data = createPayload();
             if(isBatch){
-                recordWrite(createPayload(eventsPerSec*batchSize), stats::recordTime);
+                recordWrite(payload, stats::recordTime);
             }
             else{
                 recordWrite(payload, stats::recordTime);
